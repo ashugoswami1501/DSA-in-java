75f7ddaba545a0783f3d0758c12683d7730e025a
Ashu Goswami <ashugoswami1501@gmail.com>
2026-08-05 20:50:10 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (7.52%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int len = nums.length;
-        for(int i = n; i < len; i++) {
-            nums[i] = (nums[i] << 10) | nums[i - n];
+        int arr[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            if(i%2==0){
+                arr[i]=nums[i/2];
+            }
+            else{
+                arr[i]=nums[n+(i/2)];
+            }
         }
-        int index = 0;
-        for(int i = n; i < len; i++, index += 2) {
-            nums[index] = nums[i] & 1023;
-            nums[index + 1] = nums[i] >>> 10;
-        }
-        return nums;
+        return arr;
     }
 }

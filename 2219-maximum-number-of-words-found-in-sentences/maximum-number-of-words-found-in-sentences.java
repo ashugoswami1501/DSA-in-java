4467c40e3e7b44f49c90df9887b1db03643ab35c
Ashu Goswami <ashugoswami1501@gmail.com>
2026-08-18 20:09:00 +0530

    Time: 3 ms (86.85%) | Memory: 46.3 MB (24.66%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
@@ -3,15 +3,8 @@ class Solution {
         int n=sentences.length;
         int maxcount=0;
         for(int i=0;i<n;i++){
-            int count=1;
-            for(int j=0;j<sentences[i].length();j++){
-                if(sentences[i].charAt(j)==' '){
-                    count++;
-                }       
-                if(count>maxcount){
-                    maxcount=count;
-                }
-            }
+            int words=sentences[i].split(" ").length;
+            maxcount=Math.max(maxcount,words);
         }
         return maxcount;
     }
